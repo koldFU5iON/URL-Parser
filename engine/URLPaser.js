012a96f7346e387af178a4c08adaedf55741e597
Devon Stanton <DevonS@devons-macbook-pro.home>
2018-11-28 23:08:47 +0100

    changed function to RegExp

--- engine/URLPaser.js
+++ engine/URLPaser.js
@@ -1,54 +1,21 @@
 'use strict'
 
-let urlInput = 'https://dummysite.com/news/article/';
-
 // console.log('running with url: ' + urlInput);
 
 function WebAddress(url){
-  this.url = url;
+  let regex = /(.*):\/\//g;
+  let found = url.match(regex)
 
+  this.url = url
+  this.protocol = regex.group.protocol
 }
 
-WebAddress.prototype.protocol;
-WebAddress.prototype.domain;
-WebAddress.prototype.file;
+function urlParser(url) {
+  let link = new WebAddress(url)
 
-function urlParse(url) {
-  let web = new WebAddress(url)
-
-  web.url = url
-
-  let webSeg = extractURL(web.url)
-
-  web.protocol = webSeg[0];
-  web.domain = webSeg[1];
-  web.file = webSeg[2];
-
-  console.log(`The Web address contains the following: ${web.domain} and is titled: ${web.file}`);
-
-  return web;
-}
-
-function extractURL(url) {
-let addressArray = [];
-
-  // check for protocol
-  if(url.startsWith('http:')) {
-    addressArray[0] = 'http://'
-  } else if (url.startsWith('https://')) {
-    addressArray[0] = 'https://'
-  }
-  // check for Domain
-  addressArray[1] = url.slice(url.indexOf('/',1) + 2, url.indexOf('/', 9));
-
-  // check for file
-  addressArray[2] = url.slice(url.lastIndexOf('/') + 1);
-
-  // console.log(addressArray)
-  return addressArray;
+  return link.url;
 }
 
 module.exports = {
-  urlParse: urlParse,
-  extractURL: extractURL
+  urlParser: urlParser
 };
--- test/test.js
+++ test/test.js
@@ -9,33 +9,11 @@ let urlTest = 'https://stackoverflow.com/questions/8511281/check-if-a-value-is-a
 
 describe('Testing urlParser function', function() {
   describe('urlParser function', function() {
-   //  //inserting test string
-   //  beforeEach(() => {
-   //   url.urlParse(urlTest)
-   //   // console.log(url.url);
-   // });
-
-
     it(`ensure that the returned function is calling the URL object key for 'url'`, function() {
-      expect(url.urlParse(urlTest)).to.contain.key('url');
+      expect(url.urlParser('hello')).to.equal('hello')
     });
-    it(`If it's not a URL should through an errors`)
-    it(`extractURl should return an array`, () => {
-      expect(url.extractURL(urlTest)).to.be.an('array')
-    });
-    it(`extractURl should return an array length of 3`, () => {
-      expect(url.extractURL(urlTest)).to.be.an('array').with.length(3)
-    });
-    it(`extractURl should return an array`, () => {
-      expect(url.extractURL(urlTest)).to.be.an('array')
-    });
-    it(`Returned array should not be empty`, () => {
-      expect(url.extractURL(urlTest)).to.not.contain('')
-    });
-    it(`Identify the protocol`, () => {
-      expect(url.extractURL(urlTest)).to.contain('https://')
-    });
-    it(`Identify the Domain name`)
-    it(`Identify the title(file name)`)
+    it('Check if RegExp will return the protocol of the URL', () => {
+      expect(url.urlParser(url)).to.equal('https://')
+    })
   });
 });
