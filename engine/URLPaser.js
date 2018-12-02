45a3f7ba3c4f63b13c14b60425aff33bdeaa8ffe
Devon Stanton <DevonS@devons-macbook-pro.home>
2018-12-02 18:46:38 +0100

    Parser can now seperate a sub domain

--- engine/URLPaser.js
+++ engine/URLPaser.js
@@ -6,15 +6,27 @@ function WebAddress(url){
   const regex = /(https?):\/\/(\w*)\.*(\w+)?\.(\w+)\/?/g;
   const match = regex.exec(url);
 
-  this.url = match[0].toString();
+  this.url = match[0];
   this.protocol = match[1]
-  this.domain = match[2]
-  // console.log(match)
+
+  //console.log(`Match 3 of ${url} has the domain ${match[2]} and the sub-domain of ${match[3]}`)
+
+  // this.domain = match[2]
+  // this.subDomain = match[3]
+
+  if(match[3] === undefined){
+    console.log('sub domain is empty')
+    this.subDomain = '';
+    this.domain = match[2]
+  } else {
+    this.subDomain = match[2]
+    this.domain = match[3]
+  }
+
 }
 
 function parse(url) {
   let link = new WebAddress(url)
-  console.log(link.protocol);
   return link;
 }
 
--- test/test_URLParser.js
+++ test/test_URLParser.js
@@ -3,28 +3,33 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const urlParser = require('../engine/URLPaser.js');
+const urlParser = require('../engine/URLPaser.js').parse;
 
-let urlTest = 'https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript'
+let urlTest = {
+  basic : 'https://stackoverflow.com/',
+  subDomain : 'https://sub.domain.com/'
+}
 
 describe('Testing the WebAddress Function', function() {
   describe('Testing the object', function() {
     // let urlObject = new WebAddress(urlTest)
     it('Expect urlParser to return an object', () => {
-      expect(urlParser.parse(urlTest)).to.be.a('object')
+      expect(urlParser(urlTest.basic)).to.be.a('object')
     })
     it('urlParser should return the protocol "https"', () => {
-      expect(urlParser.parse(urlTest)).to.contain.a.property('protocol').to.equal('https')
+      expect(urlParser(urlTest.basic)).to.contain.a.property('protocol').to.equal('https')
     })
-    it('urlPasrse should provide the domain name "stackoverflow"', function() {
-      expect(urlParser.parse(urlTest)).to.contain.a.property('domain').to.equal('stackoverflow')
+    it(`expect urlParser to return the domain name for ${urlTest.basic}`, function() {
+      expect(urlParser(urlTest.basic)).to.contain.a.property('domain').to.equal('stackoverflow')
     })
-    it('urlParser should provide a list of folders as an array')
+    it(`expect urlParser to return the sub-domain name for ${urlTest.subDomain}`, function() {
+      expect(urlParser(urlTest.subDomain)).to.contain.a.property('subDomain').to.equal('sub')
+    })
     it('urlParser should provide the final file')
   });
   describe('parse function', function() {
     it('Should returnt the same URL back', () => {
-      expect(urlParser.parse(urlTest)).to.contain.a.property('url').to.equal(urlTest)
+      expect(urlParser(urlTest.basic)).to.contain.a.property('url').to.equal(urlTest.basic)
     })
   });
 });
