426e1ee66d830b70597b8f63c4082d91dfe245d6
dstanton <dstanton@blizzard.com>
2018-12-04 12:58:25 +0100

    code(URLParse.pug) added input page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const URLparse = require('../engine/URLPaser');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/url/parse', (req, res, next) => {
+  let filename = '';
+  if(req.query.url_field) {
+    let link = new URLparse(req.query.url_field);
+    filename = link.file.clean;
+  }
+  res.render('URLparse', {
+    title: 'URL Parser',
+    url: req.query.url_field,
+    urlFilename: filename
+  });
+})
 
 module.exports = router;
